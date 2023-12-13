1b83bf815be8e47490821bcdb34e872d21b5339c
Peter Malovrh <121298565+petermalovrh@users.noreply.github.com>
2023-12-13 11:20:54 +0100

    13.12.2023 v0.15 podatki Portorož, Kočevje

--- tempStat.js
+++ tempStat.js
@@ -1,6 +1,6 @@
 //------------------------------------
 //---- pričetek razvoja 2.12.2023
-const gl_versionNr = "v0.14"
+const gl_versionNr = "v0.15"
 const gl_versionDate = "13.12.2023"
 const gl_versionNrDate = gl_versionNr + " " + gl_versionDate
 //------------------------------------
@@ -88,7 +88,7 @@ addPlace("Ljubljana Be"+scZhLow+"igrad (299m)", "LJ Be"+scZhLow+"igrad", "LJBE",
 stationID += 1; const cv_placeNovoMesto = stationID;  //5; // NOVO MESTO (220m) (lon=15.1773, lat=45.8018, viš=220m) ... 6.12.2023
 addPlace("Novo Mesto (220m)", "Novo Mesto", "NM", "crimson");
 stationID += 1; const cv_placePostojna = stationID;  //6; // POSTOJNA (533m) (lon=14.1932, lat=45.7661, viš=533m) ... 7.12.2023
-addPlace("Postojna (533m)", "Postojna", "PO", "goldenrod");
+addPlace("Postojna (533m)", "Postojna", "POST", "goldenrod");
 //const cv_placeNovaGoricaBilje51m = 9; // NOVA GORICA BILJE (51m) (lon=13.6315, lat=45.8911, viš=51m) ... 8.12.2023
 //addPlace("Nova Gorica Bilje (51m)", "NG Bilje", "NGBI51", "darkSalmon");
 //const cv_placeNovaGoricaBilje55m = 10; // NOVA GORICA BILJE (55m) (lon=13.6240, lat=45.8956, viš=55m) ... 8.12.2023
@@ -118,6 +118,10 @@ addPlace("Celje (245m)", "Celje", "CE", "blueViolet");
 stationID += 1; const cv_placeBabnoPolje = stationID;  //14; // BABNO POLJE ... 13.12.2023
 //    podatki postaje so v treh delih, ki sem jih združil spodaj, spodaj glej id-je in podatke konkretnih postaj
 addPlace("Babno Polje (755m)", "Babno Polje", "BABP", "darkGoldenrod");
+stationID += 1; const cv_placePortorozLetalisce = stationID;  //15; // PORTOROŽ - LETALIŠČE (lon=13.6160, lat=45.4753, viš=2m) ... 13.12.2023  id=1896
+addPlace("Portoro" + scZhLow + " Letali" + scSchLow + scTchLow + "e (2m)", "Portoro" + scZhLow, "PRT" + scZh, "deepSkyBlue");
+stationID += 1; const cv_placeKocevje = stationID;  //16; // KOČEVJE  ... 13.12.2023  id=1896
+addPlace("Ko" + scTchLow + "evje (467m)", "Ko" + scTchLow + "evje", "KO" + scTch + "E", "indianRed");
 
 //---- pomočnik za pripravo vzorca za novo postajo, rezultat v debug konzoli
 //genPlaceTemplate("cv_placePostojna", 1950, 2022)
@@ -1191,6 +1195,129 @@ addAvgTempYear(cv_placeBabnoPolje, 2019, 1, [-2.9, 0.3, 3.2, 6.5, 8.3, 18.6, 17.
 addAvgTempYear(cv_placeBabnoPolje, 2020, 1, [-0.8, 2.6, 2.3, 6.8, 10.6, 14.8, 16.2, 16.8, 11.7, 7.4, 2.2, 0.7]);
 addAvgTempYear(cv_placeBabnoPolje, 2021, 1, [-2.5, 2.3, 1.2, 3.6, 9.9, 17.5, 18.5, 16, 11.8, 5.9, 3.2, -0.6]);
 addAvgTempYear(cv_placeBabnoPolje, 2022, 1, [-2.5, 0.8, 0.5, 5.7, 13.2, 18.4, 18.6, 17.3, 11.6, 9.7, 4.3, 2.5]);
+
+//id=1896  PORTOROŽ - LETALIŠČE (lon=13.6160, lat=45.4753, viš=2m)
+addAvgTempYear(cv_placePortorozLetalisce, 1988, 6, [19.4, 23.9, 22.5, 17.3, 14.8, 5.7, 4.5]);
+addAvgTempYear(cv_placePortorozLetalisce, 1989, 1, [3.1, 6, 9.3, 12.6, 16.6, 19.1, 22.2, 21.1, 17.1, 12, 7.3, 5.6]);
+addAvgTempYear(cv_placePortorozLetalisce, 1990, 1, [3.5, 7.1, 9.1, 11.4, 17.4, 20.4, 22.2, 21.4, 16.8, 14.7, 9.5, 4.7]);
+//addAvgTempYear(cv_placePortorozLetalisce, 1991, 1, [4, 2.2, 9.9, , , , , 22.5, 19.8, 12.5, 8.3, 1.7]); //manjkajoči apr-jul, nadomestim s povprečjem iz prejšnjega in naslednjega podatka
+addAvgTempYear(cv_placePortorozLetalisce, 1991, 1, [4, 2.2, 9.9, 12, 18, 20.2, 22.2, 22.5, 19.8, 12.5, 8.3, 1.7]);   //manjkajoči apr-jul, nadomestim s povprečjem iz prejšnjega in naslednjega podatka
+addAvgTempYear(cv_placePortorozLetalisce, 1992, 1, [4.1, 3.6, 7.1, 12.6, 18.5, 20.1, 22.2, 23.7, 17.5, 13.8, 10.2, 6.1]);
+addAvgTempYear(cv_placePortorozLetalisce, 1993, 1, [3.7, 3.3, 5.9, 11.4, 18.2, 20.8, 21.4, 23, 17.9, 14.7, 7.8, 6.7]);
+addAvgTempYear(cv_placePortorozLetalisce, 1994, 1, [6.5, 4.9, 10, 11.8, 16.4, 20.7, 25, 24.2, 19.4, 12.8, 10.9, 6.4]);
+addAvgTempYear(cv_placePortorozLetalisce, 1995, 1, [4.6, 6.7, 7.2, 11.2, 16.1, 18.6, 23.8, 20.7, 16.7, 13.9, 8.6, 6]);
+addAvgTempYear(cv_placePortorozLetalisce, 1996, 1, [5.7, 3.6, 5.6, 11.9, 16.9, 20.8, 20.9, 21.3, 15.2, 13.5, 11.1, 5.4]);
+addAvgTempYear(cv_placePortorozLetalisce, 1997, 1, [6.3, 6.2, 8.9, 9.6, 17.2, 21, 21.6, 21.7, 18.5, 13.4, 9.7, 6.8]);
+addAvgTempYear(cv_placePortorozLetalisce, 1998, 1, [6, 6.3, 7.3, 12.5, 16.7, 21.2, 23.1, 23.9, 17.7, 13.7, 7.5, 4.1]);
+addAvgTempYear(cv_placePortorozLetalisce, 1999, 1, [4.6, 4, 9.3, 12.7, 17.9, 20.8, 23.2, 23.4, 20.2, 14.5, 8.2, 5.2]);
+addAvgTempYear(cv_placePortorozLetalisce, 2000, 1, [2.3, 5.1, 8.2, 13.6, 18.2, 22, 21.7, 23.1, 18.5, 15.6, 11.9, 8.4]);
+addAvgTempYear(cv_placePortorozLetalisce, 2001, 1, [7.4, 6.9, 11.3, 11.6, 18.8, 20, 23.3, 24.4, 16.6, 15.9, 8.3, 3.3]);
+addAvgTempYear(cv_placePortorozLetalisce, 2002, 1, [2.9, 6.8, 9.8, 12.4, 17.4, 22, 23.1, 21.7, 17.3, 14.8, 12.9, 7.4]);
+addAvgTempYear(cv_placePortorozLetalisce, 2003, 1, [4.1, 2.4, 7.5, 11.4, 18.5, 24.5, 24.5, 26.1, 17.2, 12.3, 10.5, 6.7]);
+addAvgTempYear(cv_placePortorozLetalisce, 2004, 1, [3.4, 4.4, 7.1, 12.4, 15, 20.7, 22.5, 22.3, 18.7, 15.9, 9.5, 6.8]);
+addAvgTempYear(cv_placePortorozLetalisce, 2005, 1, [3.5, 3, 7, 11.3, 16.9, 21.2, 22.7, 20.2, 18.4, 13.4, 8.8, 4.6]);
+addAvgTempYear(cv_placePortorozLetalisce, 2006, 1, [3.1, 4.2, 7, 12.9, 16.8, 21.7, 25.4, 20.1, 19.3, 15.7, 10.9, 7.7]);
+addAvgTempYear(cv_placePortorozLetalisce, 2007, 1, [7.7, 7.8, 10.6, 14.7, 18.8, 22.7, 24.1, 22.2, 16.7, 12.9, 8.4, 5.1]);
+addAvgTempYear(cv_placePortorozLetalisce, 2008, 1, [6.7, 5.6, 8.7, 12.6, 17.9, 21.6, 23.5, 23, 17.7, 14.8, 9.8, 6.3]);
+addAvgTempYear(cv_placePortorozLetalisce, 2009, 1, [4.5, 5.2, 8.6, 14.1, 18.7, 21, 23.3, 24, 20, 13.2, 10.8, 5.7]);
+addAvgTempYear(cv_placePortorozLetalisce, 2010, 1, [3.5, 5.6, 7.7, 12.7, 16.8, 20.7, 23.9, 21.6, 17.5, 12.8, 10.6, 4.4]);
+addAvgTempYear(cv_placePortorozLetalisce, 2011, 1, [4.5, 5.5, 8.5, 13.7, 17.9, 21.6, 22.5, 23.6, 21, 12.6, 8.6, 7.3]);
+addAvgTempYear(cv_placePortorozLetalisce, 2012, 1, [3.5, 1.5, 9.9, 12.8, 16.6, 22.7, 25.5, 24.7, 19.8, 14.9, 11.7, 5]);
+addAvgTempYear(cv_placePortorozLetalisce, 2013, 1, [5.6, 4.8, 7.4, 13.2, 16.5, 20.5, 24.3, 23.2, 18.9, 15.3, 11.1, 6.9]);
+addAvgTempYear(cv_placePortorozLetalisce, 2014, 1, [9.4, 9.8, 10.8, 13.9, 16.2, 21.6, 21.7, 21.5, 17.9, 15.4, 13, 7.8]);
+addAvgTempYear(cv_placePortorozLetalisce, 2015, 1, [6.3, 5.7, 9.2, 12.1, 18, 21.9, 25.9, 23.7, 19.4, 14, 9.1, 6.7]);
+addAvgTempYear(cv_placePortorozLetalisce, 2016, 1, [5, 8.6, 9.7, 13.4, 16.6, 21.4, 24.5, 23, 19.9, 13.4, 10.2, 4.9]);
+addAvgTempYear(cv_placePortorozLetalisce, 2017, 1, [1.9, 7, 10.2, 12.7, 17.6, 23.3, 24.2, 24.3, 17.3, 13.7, 9.8, 5.6]);
+addAvgTempYear(cv_placePortorozLetalisce, 2018, 1, [7.4, 3.7, 8.1, 15.2, 19.3, 22.5, 24.1, 24.7, 19.5, 15.9, 11.5, 5.4]);
+addAvgTempYear(cv_placePortorozLetalisce, 2019, 1, [4.1, 6.7, 10.3, 12.9, 14.6, 24.2, 24.2, 24.4, 18.8, 15.3, 12.5, 8.1]);
+addAvgTempYear(cv_placePortorozLetalisce, 2020, 1, [5.5, 7.8, 9, 13.3, 18.1, 21, 23.2, 24, 19.5, 13.7, 9.4, 7.9]);
+addAvgTempYear(cv_placePortorozLetalisce, 2021, 1, [5, 7.6, 7.7, 11, 16, 22.9, 24.4, 23, 18.9, 12.9, 10.6, 5.9]);
+addAvgTempYear(cv_placePortorozLetalisce, 2022, 1, [4.4, 6.6, 6.7, 11.9, 18.9, 24.2, 25.9, 24.6, 18.8, 16.3, 11.1, 8.5]);
+
+
+//id=2512  KOČEVJE (lon=14.8589, lat=45.6375, viš=464m)
+addAvgTempYear(cv_placeKocevje, 1950, 1, [-4.2, 2.3, 5.3, 8.6, 15.8, 19.5, 21.8, 19.2, 14.1, 7.6, 5, -0.2]);
+//addAvgTempYear(cv_placeKocevje, 1951, 1, [1.2, 3.9, 3.7, 8.7, 13.1, , , , , , , ]);
+//id=409  KOČEVJE (lon=14.8603, lat=45.6362, viš=463m)
+//addAvgTempYear(cv_placeKocevje, 1951, 1, [, , , , , , 17.4, 18, 15.6, 7.6, 7.3, 1]);
+addAvgTempYear(cv_placeKocevje, 1951, 1, [1.2, 3.9, 3.7, 8.7, 13.1, 18.5, 17.4, 18, 15.6, 7.6, 7.3, 1]); //manjkajoči jun, nadomestim s povprečjem iz prejšnjega in naslednjega podatka
+addAvgTempYear(cv_placeKocevje, 1952, 1, [-2.5, -2.9, 2, 10.4, 12.6, 17.4, 19.6, 19.4, 12.6, 9.3, 3.6, -0.6]);
+addAvgTempYear(cv_placeKocevje, 1953, 1, [-3.6, -2.4, 1.8, 9.1, 12.3, 16, 18.1, 15.8, 14.3, 11.6, 2.4, 0.1]);
+addAvgTempYear(cv_placeKocevje, 1954, 1, [-6.7, -5, 5, 6.4, 11.6, 17.4, 16.9, 16.9, 15, 9.1, 4.4, 2]);
+addAvgTempYear(cv_placeKocevje, 1955, 1, [0.7, 1.7, -0.3, 5.5, 11, 15.3, 18, 16.7, 13.3, 8.8, 3, 2]);
+addAvgTempYear(cv_placeKocevje, 1956, 1, [2.1, -9.2, 0.7, 6.8, 12.5, 14.7, 18.4, 17.4, 15.2, 7.9, 1.3, -2]);
+addAvgTempYear(cv_placeKocevje, 1957, 1, [-4.6, 4.6, 5.5, 8, 9.1, 18.1, 17.8, 16.5, 13.2, 8.7, 1.9, 1]);
+addAvgTempYear(cv_placeKocevje, 1958, 1, [-3.3, 3.5, -0.1, 5.4, 16.4, 15.8, 18.2, 18, 13, 9.2, 4.8, 3]);
+addAvgTempYear(cv_placeKocevje, 1959, 1, [-0.9, -3.1, 6.2, 8.9, 11.8, 15.4, 18.5, 16.7, 11.8, 7.2, 5, 2.4]);
+addAvgTempYear(cv_placeKocevje, 1960, 1, [-1.3, 1.3, 3.8, 7.5, 12.7, 16.4, 16.2, 17.1, 12.3, 11, 6.5, 3]);
+addAvgTempYear(cv_placeKocevje, 1961, 1, [-1.6, 2.7, 6, 11.7, 12.6, 17.5, 16.8, 16.8, 15, 11.2, 5.3, -0.9]);
+addAvgTempYear(cv_placeKocevje, 1962, 1, [0.2, -2, -0.5, 7, 11.2, 14, 15.7, 18.1, 12.6, 8.4, 3, -4.6]);
+addAvgTempYear(cv_placeKocevje, 1963, 1, [-7, -5, 1.9, 8.8, 12.3, 16.7, 18.1, 17.7, 14.4, 8.5, 9.9, -3.5]);
+addAvgTempYear(cv_placeKocevje, 1964, 1, [-6, -0.9, 1.6, 8.5, 12.6, 17.4, 17.4, 16.3, 14, 9.3, 5, -0.3]);
+addAvgTempYear(cv_placeKocevje, 1965, 1, [-0.1, -3.8, 4, 6.4, 11.6, 16.2, 17.4, 15.2, 14, 7.4, 3.6, 2.2]);
+addAvgTempYear(cv_placeKocevje, 1966, 1, [-3.3, 6.8, 3.5, 10.5, 13, 17.4, 17.1, 16.2, 14.2, 12.9, 2.5, 0.3]);
+addAvgTempYear(cv_placeKocevje, 1967, 1, [-3.8, 0.1, 5.7, 6.7, 13.3, 15.2, 18.5, 16.8, 14.2, 10.4, 5.1, -1.5]);
+addAvgTempYear(cv_placeKocevje, 1968, 1, [-4.8, 2.8, 5.4, 11, 13.1, 15.5, 17.2, 15.7, 13.5, 9.8, 4.6, -3.4]);
+addAvgTempYear(cv_placeKocevje, 1969, 1, [-2, -1.5, 2, 7.6, 14.5, 14.8, 17, 16.2, 14.3, 7.6, 6.8, -3.9]);
+addAvgTempYear(cv_placeKocevje, 1970, 1, [0, -0.1, 1.8, 7.5, 11.4, 17.3, 17.3, 17.7, 14.2, 8.2, 6.3, -1.2]);
+addAvgTempYear(cv_placeKocevje, 1971, 1, [-0.6, 1.1, 1.2, 9.9, 14.4, 15.7, 18.1, 18.7, 11, 6.9, 3.7, -1.8]);
+addAvgTempYear(cv_placeKocevje, 1972, 1, [-2.4, 1.7, 5.9, 8.4, 12.4, 16.7, 18.1, 16.9, 10.7, 7.6, 3.8, -0.6]);
+addAvgTempYear(cv_placeKocevje, 1973, 1, [-1.5, 0.2, 2.8, 6.2, 14.4, 16.8, 18, 17.1, 14.4, 7.6, 2.4, -0.5]);
+addAvgTempYear(cv_placeKocevje, 1974, 1, [0.4, 4.7, 5.2, 7.7, 12.3, 14.7, 18, 18.6, 14.5, 4.8, 5, 0.8]);
+addAvgTempYear(cv_placeKocevje, 1975, 1, [2.6, 0, 4.9, 8.5, 14.2, 14.9, 18, 17, 16, 7.9, 3.2, -0.4]);
+addAvgTempYear(cv_placeKocevje, 1976, 1, [-0.6, -1.2, -0.8, 7.6, 12.9, 15.9, 18.1, 14.1, 12.8, 9.7, 5.6, -0.6]);
+addAvgTempYear(cv_placeKocevje, 1977, 1, [2.3, 3.7, 7.1, 7.3, 12.7, 16.3, 17.7, 16.8, 11, 10.5, 4.9, -1.5]);
+addAvgTempYear(cv_placeKocevje, 1978, 1, [-0.2, -0.4, 5, 6.5, 10.8, 15.3, 16.4, 15.8, 12.8, 8.4, 0.5, 0.3]);
+addAvgTempYear(cv_placeKocevje, 1979, 1, [-2.3, 1, 5.9, 7.5, 13.5, 17.5, 17.1, 16.1, 13.6, 8.4, 4, 2.3]);
+addAvgTempYear(cv_placeKocevje, 1980, 1, [-2.8, 2.1, 4.1, 5.8, 10.5, 15.8, 17.6, 18, 14.2, 9.1, 2.3, -2.8]);
+addAvgTempYear(cv_placeKocevje, 1981, 1, [-4.1, -0.6, 6.6, 9.1, 13, 17.1, 18.4, 17.9, 15.6, 12.6, 3.3, 0.7]);
+addAvgTempYear(cv_placeKocevje, 1982, 1, [-2.3, -1.9, 3, 5.7, 13.1, 17.6, 19.1, 18, 16.7, 11, 5.6, 2.4]);
+addAvgTempYear(cv_placeKocevje, 1983, 1, [0.3, -2.9, 4, 10.5, 14.5, 16.4, 20.4, 17.8, 14.3, 8.5, 1.4, -0.4]);
+addAvgTempYear(cv_placeKocevje, 1984, 1, [-0.9, -0.3, 3, 7.7, 11.5, 15.7, 17, 16.3, 13.4, 10.6, 4.8, 0.2]);
+addAvgTempYear(cv_placeKocevje, 1985, 1, [-6.5, -4.2, 2.9, 7.6, 13.6, 14.8, 18.9, 17.5, 13.7, 7.9, 2, 2.8]);
+addAvgTempYear(cv_placeKocevje, 1986, 1, [-0.5, -4.4, 1.5, 8.7, 15.2, 15.2, 17, 18, 12.4, 8.5, 4.5, -1.7]);
+addAvgTempYear(cv_placeKocevje, 1987, 1, [-4.6, -0.2, -0.8, 9, 11.4, 16.5, 19.3, 16.5, 16.7, 10.5, 4.2, 0.3]);
+addAvgTempYear(cv_placeKocevje, 1988, 1, [3.4, 2.2, 3.4, 8.1, 13.5, 15.4, 19.9, 17.8, 13.5, 10, 0.4, -0.3]);
+//addAvgTempYear(cv_placeKocevje, 1989, 1, [-0.4, 3.1, 6.7, 9.6, 12.7, 14.5, 18, , , , , ]);
+//id=410  KOČEVJE (lon=14.8640, lat=45.6374, viš=467m)
+//addAvgTempYear(cv_placeKocevje, 1989, 1, [, , , , , , , , , 8.8, 2.7, 2.2]);
+addAvgTempYear(cv_placeKocevje, 1989, 1, [-0.4, 3.1, 6.7, 9.6, 12.7, 14.5, 18, 17.5, 13.2, 8.8, 2.7, 2.2]); //manjkajoči avg-sep, nadomestim s povprečjem iz prejšnjega in naslednjega podatka
+addAvgTempYear(cv_placeKocevje, 1990, 1, [-0.3, 4.8, 6.7, 7.4, 13.6, 16, 17.9, 17.3, 12.9, 10.7, 4.8, -0.5]);
+addAvgTempYear(cv_placeKocevje, 1991, 1, [-0.5, -2.6, 7, 6.9, 10, 16.7, 19.9, 18.3, 15.5, 7.7, 4.6, -2.7]);
+addAvgTempYear(cv_placeKocevje, 1992, 1, [-0.6, 1.3, 4.4, 8.9, 13.6, 16.5, 18.7, 20.8, 13.9, 9, 5.9, -0.7]);
+//addAvgTempYear(cv_placeKocevje, 1993, 1, [0.1, -0.8, 3, 9, 14.8, 17.2, 18.2, 18.8, 14, 11, , ]);
+//id=1694  KOČEVJE (lon=14.8501, lat=45.6460, viš=467m)
+//addAvgTempYear(cv_placeKocevje, 1993, 1, [, , , , , , , , , , , 1.6]);
+addAvgTempYear(cv_placeKocevje, 1993, 1, [0.1, -0.8, 3, 9, 14.8, 17.2, 18.2, 18.8, 14, 11, 6.1, 1.6]); //manjkajoči nov, nadomestim s povprečjem iz prejšnjega in naslednjega podatka
+addAvgTempYear(cv_placeKocevje, 1994, 1, [1.9, 0.9, 8.2, 7.7, 13.1, 16.8, 19.5, 19.6, 15.4, 6.9, 6.4, 0.8]);
+addAvgTempYear(cv_placeKocevje, 1995, 1, [-0.6, 3.6, 2.8, 8.6, 12.8, 14.5, 19.4, 16.2, 12.1, 10.3, 3.9, 0.1]);
+addAvgTempYear(cv_placeKocevje, 1996, 1, [-2, -2.3, 0.9, 7.6, 13.8, 16.9, 16.3, 17, 11.1, 9.3, 6, -3.1]);
+addAvgTempYear(cv_placeKocevje, 1997, 1, [-1.9, 1.9, 4.1, 5.2, 13.9, 17.1, 17.2, 17, 13.4, 7.4, 4.2, 1.2]);
+addAvgTempYear(cv_placeKocevje, 1998, 1, [1.9, 2.5, 3.1, 9.3, 13.1, 17.8, 18.7, 18.5, 13.5, 9.6, 2, -4.9]);
+addAvgTempYear(cv_placeKocevje, 1999, 1, [-0.4, -1.3, 5.7, 9.4, 14, 16.8, 18.2, 18.1, 15.7, 10, 1.6, 0]);
+addAvgTempYear(cv_placeKocevje, 2000, 1, [-3.3, 1.9, 5.2, 11.1, 14.2, 18.1, 17.4, 18.8, 13.9, 11.2, 7.7, 3.5]);
+addAvgTempYear(cv_placeKocevje, 2001, 1, [2.9, 2.1, 8, 7.8, 14.9, 16, 19.1, 19.9, 11.8, 11.8, 1.5, -3.1]);
+addAvgTempYear(cv_placeKocevje, 2002, 1, [-1.8, 3.8, 6, 7.6, 14.8, 18, 18.7, 17.8, 12.1, 9.9, 8.3, 0.7]);
+addAvgTempYear(cv_placeKocevje, 2003, 1, [-2.1, -4.4, 3.7, 7.5, 15.6, 20.3, 19.8, 21.1, 12.6, 7, 6.1, 0.4]);
+addAvgTempYear(cv_placeKocevje, 2004, 1, [-1.9, 0.1, 1.7, 8.4, 11.5, 16.1, 18.2, 17.9, 13.3, 11.5, 4.4, 0.3]);
+addAvgTempYear(cv_placeKocevje, 2005, 1, [-2.2, -3.6, 1.7, 8.4, 13.5, 16.9, 18.2, 16, 14.2, 10, 3.9, -0.4]);
+addAvgTempYear(cv_placeKocevje, 2006, 1, [-3.3, -1.5, 2.5, 9.1, 13.1, 17.1, 20.5, 15.7, 14.8, 11.1, 6.9, 3]);
+addAvgTempYear(cv_placeKocevje, 2007, 1, [3.1, 4.4, 5.9, 11.1, 14.8, 18.6, 18.9, 17.8, 11.7, 7.8, 3.3, -1.4]);
+addAvgTempYear(cv_placeKocevje, 2008, 1, [1.6, 2.4, 4, 8.5, 13.8, 17.8, 18.9, 17.9, 12.9, 10.1, 5.5, 0.9]);
+addAvgTempYear(cv_placeKocevje, 2009, 1, [-2.8, 0.5, 4.6, 10.6, 15.6, 16.6, 18.8, 19.1, 14.9, 8.9, 6.2, 1.4]);
+addAvgTempYear(cv_placeKocevje, 2010, 1, [-2.9, -0.4, 3.4, 9, 13.1, 17.5, 20, 17.6, 12.3, 7.7, 6.5, -1.9]);
+addAvgTempYear(cv_placeKocevje, 2011, 1, [0.2, -1, 4.2, 10.4, 13.5, 17.4, 17.9, 19.7, 16.2, 8, 2.4, 2.4]);
+addAvgTempYear(cv_placeKocevje, 2012, 1, [-0.4, -4.6, 6.5, 9.5, 13.3, 19, 20.2, 19.8, 14.4, 9.6, 7.9, -0.8]);
+addAvgTempYear(cv_placeKocevje, 2013, 1, [-0.2, -0.8, 1.5, 9.6, 13, 16.9, 19.7, 18.9, 13.7, 11.3, 5.7, 1.9]);
+addAvgTempYear(cv_placeKocevje, 2014, 1, [4, 3.5, 7, 10.7, 12.9, 16.7, 18.3, 17.5, 13.9, 11.8, 7.8, 2]);
+addAvgTempYear(cv_placeKocevje, 2015, 1, [1.4, -0.1, 4.8, 8.8, 14.8, 17.7, 21.1, 19.3, 14.4, 9.3, 4.8, 1.7]);
+addAvgTempYear(cv_placeKocevje, 2016, 1, [0.3, 3.8, 4.7, 9.9, 13, 17.4, 20.2, 17.6, 15.2, 8.5, 5.8, -1.4]);
+addAvgTempYear(cv_placeKocevje, 2017, 1, [-5.1, 3, 6.8, 9.4, 14, 18.8, 20.2, 20.2, 12.5, 9.2, 4.9, 1.2]);
+addAvgTempYear(cv_placeKocevje, 2018, 1, [3.7, -2.4, 2.3, 12.5, 15.3, 17.6, 19, 19.5, 14.5, 11.2, 6.2, 0.5]);
+addAvgTempYear(cv_placeKocevje, 2019, 1, [-1, 2.5, 6.2, 9.4, 10.7, 20.3, 19.6, 19.8, 14, 11.1, 7.5, 3]);
+addAvgTempYear(cv_placeKocevje, 2020, 1, [0.7, 5, 4.9, 10, 12.9, 17.2, 18.7, 19.6, 14.6, 9.9, 3.8, 1.9]);
+addAvgTempYear(cv_placeKocevje, 2021, 1, [-0.4, 4, 3.7, 6.6, 12, 19.9, 20.5, 18, 14.3, 7.7, 4.7, 1.2]);
+addAvgTempYear(cv_placeKocevje, 2022, 1, [-0.5, 2.7, 3.2, 7.9, 15.3, 20.2, 20.5, 19.3, 13.6, 12.3, 6.1, 3]);
 
 //---- podatki so vpisani, zdaj je treba za vsako lokacijo določiti offset, se pravi za koliko mesecev naprej od splošno prvega podatka se začnejo podatki posamezne lokacije
 for (place = 1; place <= nrPlaces; place++) {
